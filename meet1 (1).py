b0407897e823a5e881210bf1f6d92971be0f0092
hafzanwazani <hafzanwazani03@gmail.com>
2025-04-17 12:35:49 +0800

    Add files via upload

---
+++ meet1 (1).py
@@ -0,0 +1,37 @@
+# Hasil <
+a = 24
+b = 68
+hasil = a < b
+print(hasil)
+
+# Hasil >
+a = 24
+b = 68
+hasil = a > b
+print(hasil)
+
+# Hasil >=
+a = 24
+b = 68
+hasil = a >= b
+print(hasil)
+
+# Hasil <=
+a = 24
+b = 68
+hasil = a <= b
+print(hasil)
+
+# Hasil !=
+a = 24
+b = 68
+hasil = a != b
+print(hasil)
+
+# Hasil not
+a = 24
+b = 68
+if not a != b:
+    print("Nilai a sama dengan nilai b")
+else:
+    print("Nilai a tidak sama dengan nilai b")
